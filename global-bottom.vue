9f14bfdb07800b3250465e12504e470bf12d1daf
Vanilla <osu_Vanilla@126.com>
2021-10-26 18:14:12 +0800

    [+] global-bottom.vue: currentPage / total footer.

---
+++ global-bottom.vue
@@ -0,0 +1,6 @@
+<!-- https://sli.dev/custom/global-layers.html#example -->
+<template>
+  <footer class="absolute bottom-0 right-0 p-7">
+    {{ $slidev.nav.currentPage }} / {{ $slidev.nav.total }}
+  </footer>
+</template>
